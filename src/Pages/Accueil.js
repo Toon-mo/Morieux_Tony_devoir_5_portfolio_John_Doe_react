e468623e9d7cd36311445e290ce4ad08eb9b3330
MorieuxTony <tmorieux@gmail.com>
2025-05-06 17:02:35 +0200

    Creation du component Homeheader et ajout à la page d'accueil

--- src/Pages/Accueil.js
+++ src/Pages/Accueil.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import HomeHeader from "../components/HomeHeader";
 
 const Accueil = () => {
   return (
     <div>
       <Navigation />
+      <HomeHeader />
       <Footer />
     </div>
   );
