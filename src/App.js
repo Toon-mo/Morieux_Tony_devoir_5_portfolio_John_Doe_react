09219b3b782940845ac817e311089b66d6ca60c4
MorieuxTony <tmorieux@gmail.com>
2025-05-06 16:22:01 +0200

    ajout de bibliothèque react-router et créations des pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Accueil from "./Pages/Accueil";
+import Contact from "./Pages/Contact";
+import Mentions from "./Pages/Mentions";
+import Services from "./Pages/Services";
+import Realisations from "./Pages/Realisations";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/mentions" element={<Mentions />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/realisations" element={<Realisations />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Pages/Mentions.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Mentions = () => {
+  return (
+    <div>
+      <h1>Mentions Légales</h1>
+    </div>
+  );
+};
+
+export default Mentions;
+<h1>Mentions Légales</h1>;
